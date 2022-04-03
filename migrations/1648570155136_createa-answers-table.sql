61d57e19590d4441dfc3e8ad98133896167e950c
barqus <armandas0707@gmail.com>
2022-04-03 16:02:33 +0300

    finished match history

--- migrations/1648570155136_createa-answers-table.sql
+++ migrations/1648570155136_createa-answers-table.sql
@@ -2,7 +2,7 @@
 CREATE TABLE IF NOT EXISTS answers (
     id SERIAL PRIMARY KEY,
     answer TEXT,
-    question_id INTEGER NOT NULL UNIQUE,
+    question_id INTEGER NOT NULL,
     CONSTRAINT fk_question FOREIGN KEY(question_id) REFERENCES questions(id)  ON UPDATE CASCADE ON DELETE CASCADE,
     participant_id INTEGER NOT NULL,
     CONSTRAINT fk_participant FOREIGN KEY(participant_id) REFERENCES participants(id)  ON UPDATE CASCADE ON DELETE CASCADE
